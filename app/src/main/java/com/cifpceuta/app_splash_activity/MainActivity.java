caefc94d82d20e285fe5fb047c6cd2a7cfc7e267
tarde <266@cifpceuta.es>
2023-11-23 16:50:17 +0100

    Final_Splash_&_Font

---
+++ app/src/main/java/com/cifpceuta/app_splash_activity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.cifpceuta.app_splash_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+              super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+    }
+}
